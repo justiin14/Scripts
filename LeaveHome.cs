d6582ef857f222bced1cd4f3eff1d10f7b07ec65
justiin14 <justinianmihai@gmail.com>
2023-01-04 01:32:38 +0200

    Added FPS Freeze position script

--- LeaveHome.cs
+++ LeaveHome.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -29,6 +27,10 @@ public class LeaveHome : MonoBehaviour
         else
         {
             canvas.SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
